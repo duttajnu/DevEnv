a3f66765a44f351131ff7bd35770fe72a4adf11d
vdutta <vivek.dutta@iopsys.eu>
2021-02-08 12:20:40 +0530

    Added vim plugins for dev environment

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+total_setups=2
+current=1
 
-echo "Installing hooks in bashrc"
+echo "Installing hooks in bashrc (${current}/${total_setups})"
 path=$(pwd)
 
 cat >~/.bashrc <<MUTE
@@ -8,5 +10,11 @@ if [ -f ${path}/scripts/work_init.sh ]; then
 	. ${path}/scripts/work_init.sh
 fi
 MUTE
-
+current=$(( current + 1 ))
 echo "Installing hooks ... Done"
+
+echo "Installing vim plugins (${current}/${total_setups})"
+cp -rf .vim ~/
+cat .vimrc >~/.vimrc
+current=$(( current + 1 ))
+echo "Installing vim plugins .. Done"
